d5ba537b84001e9b049480b4050468a81810d9e5
George Griffiths <george.griffiths@immersivelabs.com>
2025-01-18 15:59:13 +0000

    clean up invalid cmds

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,15 +4,18 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
 )
 
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	commandInput, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-	trimmedInput := strings.TrimSpace(commandInput)
+	commandInput, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading input:", err)
+		os.Exit(1)
+	}
+	trimmedInput := commandInput[:len(commandInput)-1]
 	if trimmedInput != "" {
 		fmt.Fprintf(os.Stdout, "%s: command not found", trimmedInput)
 	}
