8ae21df0d9545fdbdb8974974ffc231dbeb62e5e
George Griffiths <george.griffiths@immersivelabs.com>
2025-01-18 16:02:29 +0000

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,16 +7,19 @@ import (
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	commandInput, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading input:", err)
-		os.Exit(1)
-	}
-	trimmedInput := commandInput[:len(commandInput)-1]
-	if trimmedInput != "" {
-		fmt.Fprintf(os.Stdout, "%s: command not found", trimmedInput)
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		commandInput, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			os.Exit(1)
+		}
+		trimmedInput := commandInput[:len(commandInput)-1]
+		if trimmedInput != "" {
+			fmt.Fprintf(os.Stdout, "%s: command not found", trimmedInput)
+		}
+		fmt.Fprintln(os.Stdout)
 	}
 }
