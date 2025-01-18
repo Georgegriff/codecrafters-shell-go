2fa24fc969bf7bd67bff7f8375f1d70ee8d34f7b
George Griffiths <george.griffiths@immersivelabs.com>
2025-01-18 15:56:07 +0000

    handle invalid input

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,12 +4,16 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
-
 
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	commandInput, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	trimmedInput := strings.TrimSpace(commandInput)
+	if trimmedInput != "" {
+		fmt.Fprintf(os.Stdout, "%s: command not found", trimmedInput)
+	}
 }
